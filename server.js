b3337ca2741e727b8b1a9221db3611d84f23abc6
Failattu <kalle.sirkesalo@eficode.com>
2015-05-04 17:34:04 +0300

    current files

--- server.js
+++ server.js
@@ -6,11 +6,12 @@ global.config = require("./config");
 global.db = require("./databases/"+config.db_driver); // This is a bit of a hack and outdated way of doing things...
 
 // Required node modules
-var restify = require("restify");
-var restifyOAuth2 = require("restify-oauth2");
+global.restify = require("restify");
+global.restifyOAuth2 = require("restify-oauth2");
 var cluster = require('cluster');
-var hooks = require("./hooks");
-
+var hooks = require("./hooks/hooks");
+var events = require("./hooks/event");
+var initial = require("./hooks/initial");
 // Process variables
 
 var server = restify.createServer({
@@ -29,7 +30,7 @@ if (process.argc >= 2) {
 	port = parseInt(process.argv[2]); // Port can be also passed as an argument, this overrides config...
 }
 
-var RESOURCES = Object.freeze({
+global.RESOURCES = Object.freeze({
 	INITIAL: "/",
 	TOKEN: "/login",
 	EVENT: "/event"
@@ -38,59 +39,10 @@ var RESOURCES = Object.freeze({
 server.use(restify.authorizationParser());
 server.use(restify.bodyParser({ mapParams: false }));
 	restifyOAuth2.cc(server, { tokenEndpoint: RESOURCES.TOKEN, hooks: hooks });
-server.get(RESOURCES.INITIAL, function (req, res) {
-	var response = {
-		_links: {
-			self: { href: RESOURCES.INITIAL }
-		}
-	};
+	
+server.get(RESOURCES.INITIAL, initial.initial);
 
-	if (req.clientId) {
-		response._links["http://rel.example.com/event"] = { href: RESOURCES.EVENT };
-	} else {
-		response._links["oauth2-token"] = {
-			href: RESOURCES.TOKEN,
-			"grant-types": "client_credentials",
-			"token-types": "bearer"
-		};
-	}
-
-	res.contentType = "application/hal+json";
-	res.send(response);
-});
-
-server.post(RESOURCES.EVENT, function (req, res) {
-	if (!req.clientId) {
-		return res.sendUnauthenticated();
-	}
-	var fields = [
-		req.body.DeviceId,
-		req.body.Description,
-		req.body.ApiKey
-	];
-	var response = {
-		"success": "event recorded successfully",
-		_links: {
-			self: { href: RESOURCES.EVENT },
-			parent: { href: RESOURCES.INITIAL }
-		}
-	}
-	db.createEvent(fields, function (err, result) {
-		if (err) {
-			response = {
-				"error": "err",
-				_links: {
-					self: { href: RESOURCES.EVENT },
-					parent: { href: RESOURCES.INITIAL }
-				}
-			}
-		}
-	});
-	if (req.body.message_type === 'APPLICATION_EVENT_QUIT')
-		hooks.invalidateClientToken(req.authorization.credentials);
-	res.contentType = "application/hal+json";
-	res.send(response);
-});
+server.post(RESOURCES.EVENT, events.eventlaunch);
 
 // Adding error information output, and killing process when this happens.
 process.on('uncaughtException', function (err) {
@@ -112,7 +64,7 @@ if (cluster.isMaster) {
     	console.log('Spawining new worker...');
     	cluster.fork();
     });
-} 
+}
 else {
     server.listen(port);
 }
